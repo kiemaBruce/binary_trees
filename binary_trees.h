87e029e86a9bdbefb6a57f57361e8b4d77595a1c
kiemaBruce <brucekiema292@gmail.com>
2024-04-09 10:15:32 +0300

    Count the leaves in a binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -40,5 +40,6 @@ size_t b_tree_height(const binary_tree_t *tree, const binary_tree_t *trav,
 size_t path_length, size_t height);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
 
 #endif /* binary_trees_h */
